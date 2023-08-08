2bb9b323981c557f364e361309a69270e988ab9d
infiniteinference <ojas@infrhq.com>
2023-08-08 21:53:49 +0530

    Event tagging & telemetry

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,26 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-
+import Script from 'next/script';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Infr',
   description: 'Machine readable catalog of everything about you.',
 };
+const GTM_ID=process.env.GTM_ID;
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+    </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
