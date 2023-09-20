a1458a98b36057b0197cadc7195641da50ebb63a
infiniteinference <ojas@infrhq.com>
2023-09-20 22:41:48 +0530

    Update: Improved details

--- components/LandingVideo.js
+++ components/LandingVideo.js
@@ -4,7 +4,7 @@ function Video() {
   return (
     <div className="rounded-3xl p-10 mt-10 overflow-hidden">
       <ReactPlayer
-        url="https://res.cloudinary.com/dcwz20wdd/video/upload/q_auto:eco/kite-export-2023-08-25T01_49_37.269_1_b9mzii.mp4"
+        url="https://res.cloudinary.com/dcwz20wdd/video/upload/q_auto:eco/v1695211327/new-replay_vzkrho.mp4"
         controls={true}
         width="100%"
         height="100%"
