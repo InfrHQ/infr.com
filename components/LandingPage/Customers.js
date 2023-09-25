ab0e7df7b1b6fbc6ba577cb0e0c84d60bbea48ea
infiniteinference <ojas@infrhq.com>
2023-09-26 03:25:47 +0530

    Fix: Remove double animate

--- components/LandingPage/Customers.js
+++ components/LandingPage/Customers.js
@@ -10,7 +10,7 @@ function Customers() {
         <h2 className="mb-4 lg:mb-6 text-3xl font-bold tracking-tight leading-tight text-center text-slate-500 dark:text-white md:text-4xl">
           You&apos;re in good company
         </h2>
-        <span className="mb-8 lg:mb-12 tracking-tight leading-tight text-center inline-flex animate-background-shine bg-[linear-gradient(110deg,#939393,45%,#1e293b,55%,#939393)] bg-[length:250%_100%] bg-clip-text text-xl text-slate-500 dark:text-transparent">
+        <span className="mb-8 lg:mb-12 tracking-tight leading-tight text-center inline-flex bg-[linear-gradient(110deg,#939393,45%,#1e293b,55%,#939393)] bg-[length:250%_100%] bg-clip-text text-xl text-slate-500 dark:text-transparent">
           Infr supports builders & dreamers around the world, from students to professionals
         </span>
         </div>
